647d05f03304611b54d428b0001fb4f21a0289c4
Devon Charles <devoncharles22@gmail.com>
2017-07-17 15:10:37 -0400

    integration-test update

--- test/test-for-recipes.js
+++ test/test-for-recipes.js
@@ -4,21 +4,13 @@ const {app, closeServer, runServer} = require('../server');
 const should = chai.should( );
 chai.use(chaiHttp);
 
-let server; 
-function runServer( ) {
-	const port = process.env.PORT || 8080;
-	return new Promise((resolve, reject) => {
-		server = app.listen(port, ( ) => {
-			console.log('Your app is listening on port ${port}');
-			resolve(server);
-		});
-		.on('error', err => {
-			reject(err);
-		});
-	});
-};
-
 if(require.main === module) {
+	before(function( ) {
+		return runServer( );
+	});
+	after(function( ) {
+		return closeServer( );
+	});
 	runServer( ).catch(err => console.error(err));
 };
 
